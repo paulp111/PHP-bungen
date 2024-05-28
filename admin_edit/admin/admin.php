351f820dc186de15f592901a912263c8f12715b8
paulp111 <133204904+paulp111@users.noreply.github.com>
2024-05-28 12:37:55 +0200

    Delete admin_edit directory

--- admin_edit/admin/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../inc/all.php';
-
-$images = new ImageGalleryCall($pdo);
-
-renderAdmin(__DIR__ . '/views/admin.view.php', [
-    'images' => $images->fetchAll()
-]);
